ce81a77cf9848cb1d3d8c1c3cb8492bd1fa27dfa
iamsurajbobade <iamsurajbobade@gmail.com>
2026-02-16 19:37:17 +0530

    updates

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -17,7 +17,6 @@ var (
 	Ctx     context.Context
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "utils",
 	Short: "A collection of utility scripts",
@@ -37,8 +36,6 @@ Common actions include cropping Aadhar PDFs using the 'aadhar' subcommand.`,
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
